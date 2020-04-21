217895398281edd8d5ba707603ffe2d823c9dbf2
Beryl01 <berylnegesa@gmail.com>
2020-04-21 12:56:52 +0300

    how to create and save new user and credetials

--- testing.py
+++ testing.py
@@ -2,3 +2,29 @@
 from user import User
 from credential import Credential
 import random
+def create_user(fname, lname, phone, email):
+    """
+    Function to create a new user
+    """
+    new_user = User(fname, lname, phone, email)
+    return new_user
+
+def create_credential(uname, pword, email):
+    """
+    Function to create new user credentials
+    """
+    new_credential = Credential(uname, pword, email)
+    return new_credential
+
+def save_user(user):
+    """
+    Function to save user
+    """
+    user.save_user_details()
+
+def save_cred(credential):
+    """
+    Function to save user credentials
+    """
+    credential.save_credential()
+
